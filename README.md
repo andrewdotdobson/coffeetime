8de78de736d73207322ae7964690f373905f3a7f
Andy Dobson <andy.dobson@wolffolins.com>
2018-05-11 10:25:25 +0100

    tutorial finished

--- README.md
+++ README.md
@@ -15,7 +15,25 @@ Node
 Express
 Mongo
 
+nodemon (for automated running of the node app (dev only))
+body-parser
 
 ## Running
 
-```node server.js```
+``` ~~node server.js~~ ```
+```npm run dev```
+
+
+
+
+## database
+
+DB is held at mLab under the `andrewdotdobson` account.
+
+
+To connect using the mongo shell:
+```mongo ds119070.mlab.com:19070/add-coffeetime -u <dbuser> -p <dbpassword>```
+
+To connect using a driver via the standard MongoDB URI (what's this?):
+
+```mongodb://<dbuser>:<dbpassword>@ds119070.mlab.com:19070/add-coffeetime```
--- server.js
+++ server.js
@@ -1,11 +1,61 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+//app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+const MongoClient = require('mongodb').MongoClient
+var db
+
+MongoClient.connect('mongodb://add-coffeetime-app:cawffe33_T1fE@ds119070.mlab.com:19070/add-coffeetime', (err, client) => {
+	//start the db server
+	if (err) return console.log(err)
+		db = client.db('add-coffeetime')
+	startServing();
+})
 
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+// Route to home
 app.get('/', (req, res) => {
-  res.send('Hello World')
+	
+	var cursor = db.collection('quotes').find().toArray(function(err, results) {
+		console.log(results)
+
+	})
+
+
+/*
+	var lastItem = db.collection('quotes').find().limit(1).sort({$natural:-1}).toArray(function(err, results) {
+		
+		
+		res.render('index', {title: 'Coffeetime', message: results[0].name})
+	})
+*/	
+	// render page
+	//res.sendFile(__dirname + '/index.html')
+	//res.render('index', { title: 'Andrew\'s page', message: lastEntry.name  })
+	// res.send('Hello World')
 })
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+//route to post data to the DB
+
+app.post('/quotes', (req, res) => {
+	// create collection and add the post data to it
+  db.collection('quotes').save(req.body, (err, result) => {
+  	if (err) return console.log(err) //handle any errors
+    console.log('saved to db')
+  	res.redirect('/')
+  })
 })
+
+
+
+function startServing(){
+
+	app.listen(3000, function() {
+	  console.log('listening on 3000 and base directory is ' + __dirname)
+	})
+}
